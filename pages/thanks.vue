950a785121ece42b6bc511d1758ceaf629796638
c.thenard <Ptitclementdu76>
2022-04-20 12:17:55 +0200

    make the form great again

---
+++ pages/thanks.vue
@@ -0,0 +1,15 @@
+<template>
+    <CthContainer>
+        <CthTitle mainTitle="Merci de m'avoir contacté !" subTitle="Je vous recontact le plus rapidement possible :)"/>
+    </CthContainer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
